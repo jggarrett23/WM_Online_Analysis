d13ed80ad81d8981c14404af6ccee62f0e515d2a
Jordan Garrett <jordangarrett@ucsb.edu>
2021-05-22 12:45:42 -0700

    v1.0.2

--- Hierarchical_Modeling.R
+++ Hierarchical_Modeling.R
@@ -42,14 +42,6 @@ setwd(dataDir)
 
 # ----- Helper functions ------
 
-gamma_a_b_from_omega_sigma <- function(mode, sd) {
-  if (mode <= 0) stop("mode must be > 0")
-  if (sd   <= 0) stop("sd must be > 0")
-  rate <- (mode + sqrt(mode^2 + 4 * sd^2)) / (2 * sd^2)
-  shape <- 1 + mode * rate
-  return(list(shape = shape, rate = rate))
-}
-
 
 # --- Read in Data ---
 
@@ -57,7 +49,14 @@ allSj_error.summary <- readRDS('loc_ss_respOrder_summary.RDS')
 
 loc.ss_error.summary  <- readRDS('loc_ss_summary.RDS')
 
+# have to change circular variables to numeric for WAIC comparisons
+allSj_error.summary$circ_mean <- as.numeric(allSj_error.summary$circ_mean)
+loc.ss_error.summary$circ_meanRad <- as.numeric(loc.ss_error.summary$circ_meanRad)
+
 # ------------- Modeling ------------------
+
+betaWeight_prior <- c(prior(normal(0,1)), class='b')
+
 
 ## MEAN OFFSET ##
 
@@ -75,55 +74,67 @@ Instead, it treats all the factors not specified as random as being fixed.
 # intercept only model
 mean.m0 <- brm(circ_meanRad ~ (1|Sj_Num), data=loc.ss_error.summary,
           family = von_mises,
+          pior = betaWeight_prior,
           save_pars = save_pars(all=T), seed = 123,
-          cores = 4, iter = 10000, chains = 4, warmup=500,
+          cores = 4, iter = 10000, chains = 4, warmup=1000,
           file=paste(modelDir,'meanOffset_intercept',sep='/'))
 
 # The formula Response_Error ~ 1 + Set_Size + Location + (1 + Set_Size + Location|Sj_Num)
 # Indicates that the fixed effects are set size and location, and that Individual Sjs vary randomly in terms of their intercept
 # and their effect of Set Size and Location
 
-# Set Size as fixed factor model
-mean.m1 <- brm(circ_meanRad ~ (1|Sj_Num) + (1|SetSize), data=loc.ss_error.summary,
+mean.m1 <- brm(circ_meanRad ~ (1|Sj_Num) + SetSize, data=loc.ss_error.summary,
           family = von_mises,
+          prior = betaWeight_prior,
           save_pars = save_pars(all=T), seed = 123,
-          cores = 2, iter = 10000, chains = 4, warmup=500,
-          file=paste(modelDir,'meanOffset_SS',sep='/'))
-
-mean.m2 <- brm(circ_meanRad ~ (1|Sj_Num) + (1|Location), data=loc.ss_error.summary,
-               family = von_mises,
-               save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
-               control = list(adapt_delta = 0.9,
-                              max_treedepth = 13),
-               file=paste(modelDir,'meanOffset_Loc',sep='/'))
-
-mean.m3 <- brm(circ_meanRad ~ (1|Sj_Num) + (1|SetSize + Location), data=loc.ss_error.summary,
-          family = von_mises,
-          save_pars = save_pars(all=T), seed = 123,
-          cores = 2, iter = 10000, chains = 4, warmup=500,
-          control = list(adapt_delta = 0.9,
+          cores = 2, iter = 5000, chains = 4, warmup=1000,
+          control = list(adapt_delta = 0.999,
                          max_treedepth = 13),
-          file=paste(modelDir,'meanOffset_Loc_SS',sep='/'))
+          file=paste(modelDir,'meanOffset_SS',sep='/'),
+          file_refit = 'on_change')
 
-mean.m4 <- brm(circ_meanRad ~ (1|Sj_Num) + (1|SetSize*Location), data=loc.ss_error.summary,
+mean.m2 <- brm(circ_meanRad ~ (1|Sj_Num) + Location, data=loc.ss_error.summary,
                family = von_mises,
+               prior = betaWeight_prior,
                save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
                control = list(adapt_delta = 0.9,
                               max_treedepth = 13),
-               file=paste(modelDir,'meanOffset_LocXSS',sep='/'))
+               file=paste(modelDir,'meanOffset_Loc',sep='/'),
+               file_refit = 'on_change')
+
+mean.m3 <- brm(circ_meanRad ~ (1|Sj_Num) + SetSize + Location, data=loc.ss_error.summary,
+          family = von_mises,
+          prior = betaWeight_prior,
+          save_pars = save_pars(all=T), seed = 123,
+          cores = 2, iter = 5000, chains = 4, warmup=1000,
+          control = list(adapt_delta = 0.999,
+                         max_treedepth = 13),
+          file=paste(modelDir,'meanOffset_Loc_SS',sep='/'),
+          file_refit = 'on_change')
+
+mean.m4 <- brm(circ_meanRad ~ (1|Sj_Num) + SetSize*Location, data=loc.ss_error.summary,
+               family = von_mises,
+               prior = betaWeight_prior,
+               save_pars = save_pars(all=T), seed = 123,
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
+               control = list(adapt_delta = 0.999,
+                              max_treedepth = 13),
+               file=paste(modelDir,'meanOffset_LocXSS',sep='/'),
+               file_refit = 'on_change')
 
 # fit diagnostics
 # Want to see fuzzy catapillars for the chains
 plot(mean.m0)
 plot(mean.m1)
 plot(mean.m2)
+plot(mean.m3)
+plot(mean.m4)
 
 # compare models using bayes factor
 
 meanComp.bf <- bf_models(mean.m0,mean.m1,mean.m2,mean.m3,mean.m4,denominator = 1)
-meanComp.loo <- loo_compare(mean.m0,mean.m1,mean.m2,mean.m3,mean.m4)
+meanComp.loo <- LOO(mean.m0,mean.m1,mean.m2,mean.m3,mean.m4)
 
 
 # ----- Summary of posterior distributions -------
@@ -152,39 +163,49 @@ Note, the range of MRVL can only be between 0 and 1, so might be best to use a b
 "
 mrvl.m0 <- brm(MRVL ~ (1|Sj_Num), data = loc.ss_error.summary,
                family = 'beta',
+               prior = betaWeight_prior,
                save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
-               file=paste(modelDir,'MRVL_intercept',sep='/'))
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
+               file=paste(modelDir,'MRVL_intercept',sep='/'),
+               control = list(adapt_delta = 0.999,
+                              max_treedepth = 13))
 
-mrvl.m1 <- brm(MRVL ~ (1|SetSize) + (1|Sj_Num), data = loc.ss_error.summary,
+mrvl.m1 <- brm(MRVL ~ SetSize + (1|Sj_Num), data = loc.ss_error.summary,
                family = 'beta',
+               prior = betaWeight_prior,
                save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
-               file=paste(modelDir,'MRVL_SS',sep='/'))
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
+               file=paste(modelDir,'MRVL_SS',sep='/'),
+               control = list(adapt_delta = 0.999,
+                              max_treedepth = 13))
 
-mrvl.m2 <- brm(MRVL ~ (1|Location) + (1|Sj_Num), data = loc.ss_error.summary,
+mrvl.m2 <- brm(MRVL ~ Location + (1|Sj_Num), data = loc.ss_error.summary,
                family = 'beta',
+               prior = betaWeight_prior,
                save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
                control = list(adapt_delta = 0.999,
                               max_treedepth = 13),
                file=paste(modelDir,'MRVL_Loc',sep='/'))
 
-mrvl.m3 <- brm(MRVL ~ (1|Location) + (1|SetSize) + (1|Sj_Num), data = loc.ss_error.summary,
+mrvl.m3 <- brm(MRVL ~ Location + SetSize + (1|Sj_Num), data = loc.ss_error.summary,
                family = 'beta',
+               prior = betaWeight_prior,
                save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
                control = list(adapt_delta = 0.999,
                               max_treedepth = 13),
                file=paste(modelDir,'MRVL_Loc_SS',sep='/'))
 
-mrvl.m4 <- brm(MRVL ~ (1|SetSize*Location) + (1|Sj_Num), data = loc.ss_error.summary,
+mrvl.m4 <- brm(MRVL ~ SetSize*Location + (1|Sj_Num), data = loc.ss_error.summary,
                family = 'beta',
+               prior = betaWeight_prior,
                save_pars = save_pars(all=T), seed = 123,
-               cores = 2, iter = 10000, chains = 4, warmup=500,
+               cores = 2, iter = 5000, chains = 4, warmup=1000,
                control = list(adapt_delta = 0.999,
                               max_treedepth = 13),
                file=paste(modelDir,'MRVL_LocXSS',sep='/'))
 
 mrvlComp.bf <- bf_models(mrvl.m0,mrvl.m1,mrvl.m2,mrvl.m3,mrvl.m4,denominator = 1)
-mrvlComp.loo <- loo_compare(mrvl.m0,mrvl.m1,mrvl.m2,mrvl.m3,mrvl.m4)
+mrvlComp.loo <- LOO(mrvl.m0,mrvl.m1,mrvl.m2,mrvl.m3,mrvl.m4)
+
